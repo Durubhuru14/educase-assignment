d027a4b9bc25491cc8af97bb859476e8b8cd9384
Durubhuru14 <durubhuru01@gmail.com>
2025-05-23 14:39:30 +0530

    fixed: scrolling issue in chrome for sign up page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import useGlobalState from "./hooks/useGlobalState.jsx";
 function App() {
   const globalState = useGlobalState();
   return (
-    <main className="min-h-screen min-w-screen bg-primaryBg flex justify-center">
+    <main className="h-screen w-screen bg-primaryBg flex justify-center">
       <article className="w-full max-w-sm px-4 py-8 grid">
         <Outlet context={globalState} />
       </article>
